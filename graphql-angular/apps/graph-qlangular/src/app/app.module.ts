40465b3c1d058be773c85160ca225326d661be5c
khaled mosaad <kmosaad@starways.com.sa>
2024-06-10 12:47:45 +0300

    add apollo angular package

--- graphql-angular/apps/graph-qlangular/src/app/app.module.ts
+++ graphql-angular/apps/graph-qlangular/src/app/app.module.ts
@@ -4,14 +4,32 @@ import { RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { appRoutes } from "./app.routes";
 import { NxWelcomeComponent } from "./nx-welcome.component";
-
+import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { InMemoryCache } from '@apollo/client/core';
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: "enabledBlocking" }),
+    HttpClientModule,
+    ApolloModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory(httpLink: HttpLink) {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'http://localhost:4000/graphql',
+          }),
+        };
+      },
+      deps: [HttpLink],
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
