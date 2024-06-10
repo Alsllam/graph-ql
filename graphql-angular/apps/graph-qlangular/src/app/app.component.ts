ad14c1b4a45fe0458146cc8668a320f3a70ab51d
Abdulsalam Eskef <aeskef@starways.com.sa>
2024-06-10 11:24:54 +0300

    init angular project

---
+++ graphql-angular/apps/graph-qlangular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "graphql-angular-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+})
+export class AppComponent {
+  title = "graph-qlangular";
+}
