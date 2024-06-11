4240c137553bee58caefcaabb8ecc15e4e50bd0a
khaled mosaad <kmosaad@starways.com.sa>
2024-06-11 11:03:52 +0300

    add feed list

---
+++ graphql-angular/apps/graph-qlangular/src/app/feed-details/feed-details.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'graphql-angular-feed-details',
+  templateUrl: './feed-details.component.html',
+  styleUrls: ['./feed-details.component.scss'],
+})
+export class FeedDetailsComponent {}
