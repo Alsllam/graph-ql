fa6e0a021478fcea926512251f0872dfb429e788
khaled mosaad <kmosaad@starways.com.sa>
2024-06-12 11:08:30 +0300

    add feed-details

--- graphql-angular/apps/graph-qlangular/src/app/feed-details/feed-details.component.ts
+++ graphql-angular/apps/graph-qlangular/src/app/feed-details/feed-details.component.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Apollo, QueryRef, gql } from 'apollo-angular';
+interface Comment{
+  body:string,
+  linkId:string
+}
 @Component({
   selector: 'graphql-angular-feed-details',
   templateUrl: './feed-details.component.html',
   styleUrls: ['./feed-details.component.scss'],
 })
-export class FeedDetailsComponent {}
+export class FeedDetailsComponent implements OnInit {
+  feedQuery:QueryRef<{comment: Comment}>;
+  details:Comment;
+  constructor(private activatedRoute:ActivatedRoute , private apollo:Apollo){}
+  ngOnInit(): void {
+      this.activatedRoute.params.subscribe(res=>{
+        this.getData(res['id']);
+      })
+  }
+  public getData(id:number) {
+    const GET_DATA = gql`
+      query GetComment($id: ID!) {
+        comment(id:$id){
+            body,
+        }
+      }
+    `
+    ;
+    this.feedQuery = this.apollo.watchQuery<{comment: Comment}>({ 
+      query: GET_DATA,
+      variables:{id:id},
+    });
+    this.feedQuery.valueChanges.subscribe(({data})=>{
+      this.details = data?.comment
+      console.log('this.details' , this.details);
+    })
+    
+  }
+}
