6e92413f76d5e36dbae5542ad7b140e6162e16fe
Abdulsalam Eskef <aeskef@starways.com.sa>
2024-06-23 12:21:16 +0300

    Add WebSocket on Port 5000

--- graphql-node-ts/src/event-schema.ts
+++ graphql-node-ts/src/event-schema.ts
@@ -43,6 +43,11 @@ const typeDefinitions = /* GraphQL */ `
     updateSession(id: ID!, title: String, startTime: String, endTime: String): Session
     deleteSession(id: ID!): Session
   }
+  
+  type Subscription {
+    countdown(from: Int!): Int!
+  }
+
 `
 // speakers: [Speaker]
 
@@ -168,6 +173,17 @@ const resolvers = {
           return newSession
       }
    
+    },
+    Subscription: {
+      countdown: {
+        // This will return the value on every 1 sec until it reaches 0
+        subscribe: async function* (a: any, { from }: any) {
+          for (let i = from; i >= 0; i--) {
+            await new Promise(resolve => setTimeout(resolve, 1000))
+            yield { countdown: i }
+          }
+        }
+      }
     }
   }
 
--- graphql-node-ts/src/main.ts
+++ graphql-node-ts/src/main.ts
@@ -2,6 +2,8 @@ import { createServer } from 'http'
 import { createYoga } from 'graphql-yoga'
 import { createContext } from './context'
 import { schema } from './event-schema'
+import { WebSocketServer } from 'ws'
+import { useServer } from 'graphql-ws/lib/use/ws'
 
 function main() {
     const yoga = createYoga({ schema, context: createContext })
@@ -9,6 +11,57 @@ function main() {
     server.listen(4000, () => {
         console.info('Server is running on http://localhost:4000/graphql')
     })
+
+
+    const yogaApp = createYoga({
+        schema, context: createContext,
+        graphiql: {
+            // Use WebSockets in GraphiQL
+            subscriptionsProtocol: 'WS'
+        }
+    });
+    // Get NodeJS Server from Yoga
+    const httpServer = createServer(yogaApp);
+    // Create WebSocket server instance from our Node server
+    const wsServer = new WebSocketServer({
+        server: httpServer,
+        path: yogaApp.graphqlEndpoint
+    });
+    // Integrate Yoga's Envelop instance and NodeJS server with graphql-ws
+    useServer({
+        execute: (args: any) => args.rootValue.execute(args),
+        subscribe: (args: any) => args.rootValue.subscribe(args),
+        onSubscribe: async (ctx, msg) => {
+                const { schema, execute, subscribe, contextFactory, parse, validate } = yogaApp.getEnveloped({
+                ...ctx,
+                req: ctx.extra.request,
+                socket: ctx.extra.socket,
+                params: msg.payload
+                })
+        
+                const args = {
+                schema,
+                operationName: msg.payload.operationName,
+                document: parse(msg.payload.query),
+                variableValues: msg.payload.variables,
+                contextValue: await contextFactory(),
+                rootValue: {
+                    execute,
+                    subscribe
+                }
+                }
+        
+                const errors = validate(args.schema, args.document)
+                if (errors.length) return errors
+                return args
+            }
+        },
+        wsServer
+    );
+    httpServer.listen(5000, () => {
+        console.log('Server is running on port 5000')
+    })
+
 }
 
 main()
